b36b5b7bacc529c7c604d3d4e285e3a6d508e84c
dashingshakeel <shkeelrauf@gmail.com>
2016-10-09 23:43:49 +0500

    Creating Model methods

---
+++ app5.js
@@ -0,0 +1,23 @@
+var Book =Backbone.Model.extend({
+  defaults: {
+    chapters: 5,
+    current: 1
+  },
+  urlRoot: '/books',
+
+
+ validate: function(attrs,opts){
+  if (attrs.published && typeof attrs.published !== 'number'){
+    return "publish should be a number";
+  }
+ },
+ read: function(){
+  var curr= this.get('current');
+  if (curr < this.get('chapters')){
+    this.set('current', curr+1);
+  }
+ },
+ isFinished: function(){
+  return this.get('chapters')== this.get('current');
+ }
+});
